ee41edf59fbb2dca1a6149cff650c23f957a4dbd
Binh <>
2019-06-05 16:49:00 +0200

    set layout in Main.storyboard

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Binh Phan on 05.06.19.
+//  Copyright © 2019 Binh Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
