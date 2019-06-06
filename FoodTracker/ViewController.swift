c2b20f0aa4e19979c9b7fee13cab3cc90da8129b
Binh <>
2019-06-06 16:14:22 +0200

    connect image view to UIViewController

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
