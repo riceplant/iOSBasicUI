2aed071339cd36784f61e5f8ee16031c9141d56e
Binh <>
2019-06-07 16:21:44 +0200

    added RatingControl class

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Binh Phan on 07.06.19.
+//  Copyright © 2019 Binh Phan. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
